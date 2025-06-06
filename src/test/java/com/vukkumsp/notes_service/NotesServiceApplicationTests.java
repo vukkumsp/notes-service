ab3d1316f3a4837681372c52101f6409b5c57a79
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-06 23:52:45 +0530

    initial commit

---
+++ src/test/java/com/vukkumsp/notes_service/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vukkumsp.notes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
