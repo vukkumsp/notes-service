b66f996e2abc7c5e2f7d95a5d70942e3048d7ab9
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-07 17:44:57 +0530

    excluded db config for now.

--- src/test/java/com/vukkumsp/notes_service/NotesServiceApplicationTests.java
+++ src/test/java/com/vukkumsp/notes_service/NotesServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.vukkumsp.notes_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class
+})
 class NotesServiceApplicationTests {
 
 	@Test
