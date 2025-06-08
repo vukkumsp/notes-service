c3d84ba53c15e0b1aa8c89fa58970b59ed514eda
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-08 16:25:23 +0530

    updated the db table to delete data on start and load dummy data for testing; updated the note table with new columns and updated its entity;

--- src/main/java/com/vukkumsp/notes_service/repository/Note.java
+++ src/main/java/com/vukkumsp/notes_service/repository/Note.java
@@ -3,11 +3,15 @@ package com.vukkumsp.notes_service.repository;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import java.time.LocalDateTime;
 
 @Table("note")
 @Data
 public class Note {
     @Id
     private Long id;
+    private String title;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
