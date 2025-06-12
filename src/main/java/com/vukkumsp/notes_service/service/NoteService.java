58f8d41d2bafa8dfba803b7df3f74a75a7b9367f
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-12 19:44:33 +0530

    added default spring cache to note service; added deleteNote method as well for future ref;

--- src/main/java/com/vukkumsp/notes_service/service/NoteService.java
+++ src/main/java/com/vukkumsp/notes_service/service/NoteService.java
@@ -2,6 +2,9 @@ package com.vukkumsp.notes_service.service;
 
 import com.vukkumsp.notes_service.repository.Note;
 import com.vukkumsp.notes_service.repository.NoteRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,15 +24,22 @@ public class NoteService {
         return this.noteRepo.findAll();
     }
 
+    @Cacheable(value = "notes", key = "#id")
     public Mono<Note> getNote(Long id){
         return this.noteRepo.findById(id);
     }
 
+    @CachePut(value = "notes", key = "#notes.id")
     public Mono<Note> updateNote(Note note){
         return noteRepo.save(note);
     }
 
     public Mono<Note> addNote(Note note){
         return noteRepo.save(note);
+    }
+
+    @CacheEvict(value = "notes", key = "#notes.id")
+    public Mono<Void> deleteNote(Long id){
+        return noteRepo.deleteById(id);
     }
 }
